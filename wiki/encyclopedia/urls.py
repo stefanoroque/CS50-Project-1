cdec196104fc43f0b26fe3070b7ce6aa33f74541
stefanoroque <stefanoroque98@gmail.com>
2020-07-15 14:56:43 -0400

    can now find entries through URL and Markdown file is converted to HTML and injected into entry page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.view_entry, name="view_entry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django import forms
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def view_entry(request, title):
+    entry = util.get_entry(title)
+
+    if entry is None:
+        return render(request, "encyclopedia/entry.html", {
+        "entry": None,
+        "title": title.capitalize()
+    })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdown2.markdown(entry),
+            "title": title.capitalize()
+        })
+   
